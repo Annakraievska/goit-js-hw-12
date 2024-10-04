9a752d6a3d70dcaadea7f7b392c461be8972ea43
Anna <annasharamko2606@gmail.com>
2024-10-04 16:09:12 +0300

    work

---
+++ src/pixabay-api.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+export const returnPromise = (q, page) => {
+  return axios.get('https://pixabay.com/api/', {
+    params: {
+      key: '34668694-a67447f729748c64ab9ad3b4f',
+      q,
+      orientation: 'horizontal',
+      safesearch: true,
+      page,
+      per_page: '15',
+    },
+  });
+};
